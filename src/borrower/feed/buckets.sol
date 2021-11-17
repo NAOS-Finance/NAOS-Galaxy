68b172a4c64dbc2454be3e0d64196e390cdedd35
x64x48 <x64x48@protonmail.com>
2021-11-17 15:22:56 +0800

    GLOBAL-01/Unlocked-Compiler-Version-Declaration

--- src/borrower/feed/buckets.sol
+++ src/borrower/feed/buckets.sol
@@ -11,7 +11,7 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
-pragma solidity >=0.5.15;
+pragma solidity 0.5.15;
 
 // the buckets contract stores values in a map using a timestamp as a key
 // each value store a pointer the next value in a linked list
